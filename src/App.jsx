a6544f1d323897e19574f872608e7ecb8031d056
GAURAV KESHARI <gauravkeshari@GAURAVs-Laptop.local>
2024-02-18 14:20:30 +0530

    Setup the context file to handle the state management of application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { Button, CssBaseline, ThemeProvider } from "@mui/material";
- 
-import { appTheme } from "./theme.js";
+import { Button, CssBaseline, ThemeProvider } from '@mui/material';
+
+import { appTheme } from './theme.js';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Page from '../src/pages/index.jsx';
- 
+import FoodCodeProvider, { useFoodCodeContext } from './store/Context.jsx';
+
 function App() {
-  return (
-    <ThemeProvider theme={appTheme}>
-      <CssBaseline enableColorScheme />
-      <Container sx={{padding:'20px'}}>
-        <Box sx={{ height: '50vh',width:'90vw',display:'flex'}}>
-         <Page/>
-        </Box>
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={appTheme}>
+            <CssBaseline enableColorScheme />
+            <FoodCodeProvider>
+                <Container sx={{ padding: '20px' }}>
+                    <Box sx={{ height: '50vh', width: '90vw', display: 'flex' }}>
+                        <Page/>
+                    </Box>
+                </Container>
+            </FoodCodeProvider>
+        </ThemeProvider>
+    );
 }
- 
+
 export default App;
---
+++ src/store/Context.jsx
@@ -0,0 +1,38 @@
+import { createContext, useReducer, useContext } from 'react';
+
+export const initialState = {
+    name: 'hello',
+    price: 100,
+};
+export const foodCodeReducer = (state, action) => {
+    switch (action.type) {
+        case 'update-root': {
+            const payload = action.payload;
+            let newState = { ...state };
+            for (let [key, value] of Object.entries(payload)) {
+                newState[key] = value;
+            }
+            return newState;
+        }
+    }
+};
+
+export const foodCodeContext = createContext();
+
+export const FoodCodeProvider = (props) => {
+    const [pageState, dispatch] = useReducer(foodCodeReducer, initialState);
+    return (
+        <foodCodeContext.Provider
+            value={{
+                pageState: pageState,
+                dispatch: dispatch,
+            }}
+        >
+            {props.children}
+        </foodCodeContext.Provider>
+    );
+};
+
+export default FoodCodeProvider;
+
+export const useFoodCodeContext = () => useContext(foodCodeContext);

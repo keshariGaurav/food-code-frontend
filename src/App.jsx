4f84dd495bc04691464121bbc6e0db4798c33f3b
GAURAV KESHARI <gauravkeshari@GAURAVs-Laptop.local>
2024-02-18 17:40:06 +0530

    fix import order

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { Button, CssBaseline, ThemeProvider } from '@mui/material';
 import { appTheme } from './theme.js';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+
 import Page from '../src/pages/index.jsx';
 import Login from '../src/pages/loginPage/index.jsx';
 import FoodCodeProvider, { useFoodCodeContext } from './store/Context.jsx';
@@ -13,7 +15,7 @@ function App() {
             <FoodCodeProvider>
                 <Container sx={{ padding: '20px' }}>
                     <Box sx={{ height: '50vh', width: '90vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        {/* <Page/> */}
+                        <Page />
                         <Login />
                     </Box>
                 </Container>

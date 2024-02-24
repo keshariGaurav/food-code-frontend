e18fb86186d06c76413ca43b0612a58965472ad6
GAURAV KESHARI <gauravkeshari@GAURAVs-Laptop.local>
2024-02-24 15:55:43 +0530

    fix import error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,5 @@
-import NamePriceFieldWrapper from 'src/layout/name_price_field_wrapper';
-import RadioButtonGroup from 'src/components/radio_groups';
 import FoodCodeProvider, { useFoodCodeContext } from 'src/store/Context';
-import BackButton from 'src/components/buttons/back_button';
-import UploadImage from 'src/components/upload_image';
+
 const ContainerHelper = (props) => {
     const { pageState, dispatch } = useFoodCodeContext();
     const radioLabel = ['Best Seller', 'Must Try', 'Our Speical'];

b14806c87fb3894d2c24d6c20415ffa12d537aa2
Ayushi <khandeliaayushi08@gmail.com>
2024-02-24 13:37:44 +0530

    radio group & upload component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,19 @@
 import NamePriceFieldWrapper from 'src/layout/name_price_field_wrapper';
+import RadioButtonGroup from 'src/components/radio_groups';
 import FoodCodeProvider, { useFoodCodeContext } from 'src/store/Context';
+import UploadImage from 'src/components/upload_image';
 const ContainerHelper = (props) => {
     const { pageState, dispatch } = useFoodCodeContext();
     console.log(pageState);
+    const radioLabel = ['Best Seller', 'Must Try', 'Our Speical'];
+    const formLabel = 'Special Tags';
 
-    return <NamePriceFieldWrapper />;
+    return (
+        <>
+            {/* <NamePriceFieldWrapper /> */}
+            {/* <RadioButtonGroup formLabel={formLabel} radioLabel={radioLabel} /> */}
+            <UploadImage />
+        </>
+    );
 };
 export default ContainerHelper;
